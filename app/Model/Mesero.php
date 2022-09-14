44f14d4c245c1ff1215c13da722a3ac6333ffc9c
Christian Vondrak <christian20499@gmail.com>
2022-09-14 15:16:49 -0400

    Modulo de mesoneros terminado

---
+++ app/Model/Mesero.php
@@ -0,0 +1,39 @@
+<?php 
+
+class Mesero extends AppModel{
+
+    public $validate = array(
+        'dni'=>array(
+            'notEmpty'=>array(
+                'rule' => 'notBlank'
+            ),
+            'numeric'=>array(
+                'rule'=>'numeric',
+                'message'=>'Solo numeros'
+            ),
+            'unique'=>array(
+                'rule'=>'IsUnique',
+                'message'=>'El DNI ya se encuentra en nuestra base de datos'
+            )
+        ),
+        'nombre'=>array(
+            'rule'=>'notBlank'
+        ),
+        'apellido'=>array(
+            'rule'=>'notBlank'
+        ),
+        'telefono'=>array(
+            'notEmpty'=>array(
+                'rule' => 'notBlank'
+            ),
+            'numeric'=>array(
+                'rule'=>'numeric',
+                'message'=>'Solo numeros'
+            )
+        )
+    );
+
+    
+
+
+}
